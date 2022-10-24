0bdfb4c7d1947487f7a0b39bbc29dd38ad17cb5e
aylaram <aylarameredova@gmail.com>
2022-10-24 18:32:40 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Todolist from './components/Todolist';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppBar position='static'>
+         <Toolbar>
+           <Typography variant='h5'>My ToDo List</Typography>
+         </Toolbar>
+       </AppBar>
+     <Todolist />
+  
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Todolist.js
@@ -0,0 +1,118 @@
+import React, { useState, useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function Todolist(){
+    const gridRef = useRef();
+
+    const columns = [
+        {field: 'description', sortable: true, filter: true, floatingFilter: true},
+        {field: 'date', sortable: true, filter: true, floatingFilter: true},
+        {field: 'priority', sortable: true, filter: true, floatingFilter: true, 
+        cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}},
+
+    ];
+
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState({description: '', date: '', priority: ''})
+    const addTodo = () => {
+        setTodos([...todos, todo])
+        setTodo({description: '', date: '', priority: ''});
+    }
+
+    const inputChanged = (event) => {
+        setTodo({...todo, [event.target.name]: event.target.value})
+    }
+
+    const deleteTodo = () => {
+        if(gridRef.current.getSelectedNodes().length> 0){
+    
+        setTodos(todos.filter((todo, index)=> index !== gridRef.current.getSelectedNodes()[0].childIndex))
+        }
+        else{
+            alert('Select the row')
+        }
+    }
+
+    return(
+        <div>
+            
+        <Stack 
+            direction="row" 
+            spacing={2}     
+            alignItems="center" 
+            justifyContent="center"
+            margin={5}>
+                <TextField
+                label='Description'
+                name='description'
+                value={todo.description}
+                variant="filled"
+                onChange={inputChanged}
+                />
+
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        label="Date"
+                        value={todo.date}
+                        onChange={(newValue) => {
+                        setTodo({... todo, date: newValue});
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                    </LocalizationProvider>
+
+                <TextField
+                label='Priority'
+                name = 'priority'
+                value={todo.priority}
+                variant="filled"
+                onChange={inputChanged}
+                />
+
+                <Button 
+                    variant = "contained" 
+                    endIcon = {<AddIcon />}
+                    onClick={addTodo}>
+                    Add
+                </Button>
+                <Tooltip title="Slect the row to delete">
+                    <Button 
+                        variant = "contained" 
+                        endIcon = {<DeleteIcon />}
+                        onClick={deleteTodo}>  
+                        Delete
+                    </Button>
+                </Tooltip>
+            
+           
+        </Stack>
+        
+         <div className='ag-theme-material' style={{height:400, width:700, margin: 'auto'}}>
+             <AgGridReact
+             ref = {gridRef}
+             onGridReady={params => gridRef.current = params.api}
+             rowSelection='single'
+             rowData={todos}
+             columnDefs={columns}
+             animateRows = 'true'
+             />
+         </div>
+         </div>
+    );
+}
